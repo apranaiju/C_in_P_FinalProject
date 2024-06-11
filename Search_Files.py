746eab098a41338693289ff7dc537287350407a6
apranaiju <155357864+apranaiju@users.noreply.github.com>
2024-06-11 17:38:39 +0530

    Add files via upload

---
+++ Search_Files.py
@@ -0,0 +1,6 @@
+'''                                                   SEARCH_FILES                                              '''
+
+'''SEARCH_FILES program searches the location of files, whose names are listed in a text file, from the directory.
+    In the next step, SEARCH_FILES copy those listed files from the directory and paste them in desired location'''
+
+
